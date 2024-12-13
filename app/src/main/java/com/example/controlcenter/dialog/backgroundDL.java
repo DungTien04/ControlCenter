ec54a665692567fd700c1a43cb33e7e103b2623f
DungTien04 <167021991+DungTien04@users.noreply.github.com>
2024-12-13 16:46:43 +0700

    layout-7

--- app/src/main/java/com/example/controlcenter/dialog/backgroundDL.java
+++ app/src/main/java/com/example/controlcenter/dialog/backgroundDL.java
@@ -21,12 +21,7 @@ public class backgroundDL extends DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dia_frg_background,container,false);
-        mActionCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getDialog().dismiss();
-            }
-        });
+        mActionCancel.setOnClickListener(v -> getDialog().dismiss());
 
         return view;
     }
